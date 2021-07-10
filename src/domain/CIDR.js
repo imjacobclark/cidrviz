5b81f1fcd19364b604261b5837762b1d853cd7a1
Jacob Clark <jacob.jh.clark@googlemail.com>
2021-07-10 23:11:49 +0100

    Refactoring error messages to constants

--- src/domain/CIDR.js
+++ src/domain/CIDR.js
@@ -1,16 +1,20 @@
 import OutOfBoundError from '../errors/OutOfBoundsError';
 
+
 const CIDR_RANGE_UPPER_BOUND = 32;
 const CIDR_RANGE_LOWER_BOUND = 0;
 
 const OCTET_UPPER_BOUND = 255;
 const OCTET_LOWER_BOUND = 0;
 
+const OUT_OF_BOUND_ERROR_MESSAGE_CIDR = `CIDR range must be between ${CIDR_RANGE_LOWER_BOUND} and ${CIDR_RANGE_UPPER_BOUND}`;
+const OUT_OF_BOUND_ERROR_MESSAGE_OCTET = `Octet exceeds ${OCTET_LOWER_BOUND} and ${OCTET_UPPER_BOUND}. This is an invalid IP block.`;
+
 const accountForZeroIndexing = addresses => addresses - 1;
 
 const availableAddresses = range => {
     if(range > CIDR_RANGE_UPPER_BOUND || range < CIDR_RANGE_LOWER_BOUND){
-        throw new OutOfBoundError(`CIDR range must be between ${CIDR_RANGE_LOWER_BOUND} and ${CIDR_RANGE_UPPER_BOUND}`);
+        throw new OutOfBoundError(OUT_OF_BOUND_ERROR_MESSAGE_CIDR);
     }
 
     return Math.pow(2, (CIDR_RANGE_UPPER_BOUND - range));
@@ -25,7 +29,7 @@ const firstUsableAddress = (ip, range) => {
     const octetRangeExceedsUpperBound = accountForZeroIndexing(lastOctet + numberOfPossibleAddresses) > OCTET_UPPER_BOUND;
 
     if(octetExceedsLowerBound || octetExceedsUpperBound || octetRangeExceedsUpperBound) 
-        throw new OutOfBoundError(`Octet exceeds ${OCTET_LOWER_BOUND} and ${OCTET_UPPER_BOUND}. This is an invalid IP block.`);
+        throw new OutOfBoundError(OUT_OF_BOUND_ERROR_MESSAGE_OCTET);
     
     return ip;
 }
